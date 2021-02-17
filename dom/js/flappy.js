4c19bf6efddaca5df0ddc12f11edb6ac0bc3ded9
unknown <giordano.cassini@gmail.com>
2021-02-17 12:54:30 -0300

    finalizando flappy do curso

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -108,6 +108,11 @@ function estaoSobrepostos (elementoA, elementoB) {
     const a = elementoA.getBoundingClientRect()
     const b = elementoB.getBoundingClientRect()
 
+    const horizontal = a.left + a.width >= b.left
+        && b.left + b.width >= a.left;
+    const vertical = a.top + a.height >= b.top
+        && b.top + b.height >=a.top;
+    return horizontal && vertical;
 }
 
 function FlappyBird () {
